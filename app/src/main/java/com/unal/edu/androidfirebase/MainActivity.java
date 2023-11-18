8bdb75fadd86fe8d87761a04b24e11b0f278eef6
kaninoc <47092141+kaninoc@users.noreply.github.com>
2023-11-18 10:33:07 -0500

    correct firebase addplayer1

--- app/src/main/java/com/unal/edu/androidfirebase/MainActivity.java
+++ app/src/main/java/com/unal/edu/androidfirebase/MainActivity.java
@@ -1,11 +1,22 @@
 package com.unal.edu.androidfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +27,19 @@ public class MainActivity extends AppCompatActivity {
     private ImageView image1,image2,image3,image4,image5,image6,image7,image8,image9;
 
     private TextView player1TV,player2TV;
+
+    //winning moves
+    private final List<int[]> combinationslist = new ArrayList<>();
+
+    //player unique id
+    private String playerUniqueId = "0";
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tic-tac-toe-firebase-ae6a6-default-rtdb.firebaseio.com/");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final String getPlayerName = getIntent().getStringExtra("playerName");
 
         player1Layout = findViewById(R.id.player1Layout);
         player2Layout = findViewById(R.id.player2Layout);
@@ -37,6 +55,42 @@ public class MainActivity extends AppCompatActivity {
 
         player1TV = findViewById(R.id.player1TV);
         player2TV = findViewById(R.id.player2TV);
+
+        final String getPlayerName = getIntent().getStringExtra("playerName");
+
+        //winning moves
+        combinationslist.add(new int[]{0,1,2});
+        combinationslist.add(new int[]{3,4,5});
+        combinationslist.add(new int[]{6,7,8});
+        combinationslist.add(new int[]{0,3,6});
+        combinationslist.add(new int[]{1,4,7});
+        combinationslist.add(new int[]{2,5,8});
+        combinationslist.add(new int[]{0,4,8});
+        combinationslist.add(new int[]{2,4,6});
+
+        //waiting for oponent
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("waitting for opponent");
+        progressDialog.show();
+
+        //generating player unique id, identification player for id
+        playerUniqueId = String.valueOf(System.currentTimeMillis());
+
+        //set to text view
+        player1TV.setText(getPlayerName);
+
+        databaseReference.child("connections").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
